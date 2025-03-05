b0e1a66c1c99870fce8cdd686704b63836551db5
DeepVikky88 <radheymusic51@gmail.com>
2025-03-05 20:25:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 1265397721))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -36,15 +36,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DeepVikky88/Nannu",
+    "https://github.com/DeepVikky88/RoyalQueen",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DPZ_STORE")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFriendsChatting")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/exampurss_official")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/exampurrs")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -88,13 +88,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/7wojv6.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/9t7dge.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/zk5w01.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/8ixlzx.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/7wojv6.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/8ixlzx.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
